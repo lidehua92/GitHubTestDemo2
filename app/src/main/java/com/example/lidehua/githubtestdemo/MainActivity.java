90525b2b9a0e35b8cc4eedd07239eb9aca5abe50
lidehua92 <ldh13699842835@sina.com>
2015-12-10 16:21:19 +0800

    第二次提交

--- app/src/main/java/com/example/lidehua/githubtestdemo/MainActivity.java
+++ app/src/main/java/com/example/lidehua/githubtestdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //更多符合规定
+        //zhujie2
     }
 }
