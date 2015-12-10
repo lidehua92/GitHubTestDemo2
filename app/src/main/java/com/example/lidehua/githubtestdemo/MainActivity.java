cc9ace1d18410c9857cd3bd88d9210d2e68c2708
lidehua92 <ldh13699842835@sina.com>
2015-12-10 16:05:27 +0800

    第一次提交

---
+++ app/src/main/java/com/example/lidehua/githubtestdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lidehua.githubtestdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //zhujie
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
